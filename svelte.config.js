a58684192fb72caba12ff701a868b32aef03b018
KobbyJake <appiahbaahjake@gmail.com>
2023-10-13 13:02:51 +0000

    added github pages config

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/kit/vite';
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-  kit: {
-    adapter: adapter()
-  },
-  preprocess: vitePreprocess()
-};
+import adapter from '@sveltejs/adapter-static';Cannot find module '@sveltejs/adapter-static' or its corresponding type declarations.2307Cannot find module '@sveltejs/adapter-static' or its corresponding type declarations.
+/** @type {import('@sveltejs/kit').Config} */const config = {	kit: {		adapter: adapter()	}};
+config.paths = { base: process.argv.includes('dev') ? '' : process.env.BASE_PATH }
 export default config;
